f246ff8c7ea2cfafb6e5da023731d164ff2fe148
Simon Fraguela <simon9@gmx.net>
2019-12-27 12:19:11 +0100

    first commit

---
+++ src/main/java/de/fraguela/backendcalendar/BackendCalendarApplication.java
@@ -0,0 +1,13 @@
+package de.fraguela.backendcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fraguela/backendcalendar/BackendCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fraguela.backendcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
